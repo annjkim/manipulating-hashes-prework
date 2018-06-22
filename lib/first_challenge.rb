a4a93c5e9a03a399100423328f404724b36f687b
Ann Kim <annkim822@gmail.com>
2018-06-22 10:20:52 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,22 @@ def first_challenge
   }
 
   #your code here
-
+contacts["Jon Snow"][:favorite_icecream_flavors] << "mint chip"
+ 
+puts contacts 
+#  => {
+  "Jon Snow" => {
+    name: "Jon",
+    email: "jon_snow@thewall.we", 
+    favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
+    knows: nil
+  },
+  "Freddy Mercury" => {
+    name: "Freddy",
+    email: "freddy@mercury.com",
+    favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
+  }
+}
 
   #remember to return your newly altered contacts hash!
   contacts
